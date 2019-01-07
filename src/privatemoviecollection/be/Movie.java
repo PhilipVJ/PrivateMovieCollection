ac0a84fe5ffb7181848905a10163c08077e51d52
PhilipVJ <44639834+PhilipVJ@users.noreply.github.com>
2019-01-07 11:57:58 +0100

    movie

--- src/privatemoviecollection/be/Movie.java
+++ src/privatemoviecollection/be/Movie.java
@@ -5,11 +5,52 @@
  */
 package privatemoviecollection.be;
 
+import java.util.Date;
+
 /**
  *
  * @author Philip
  */
 public class Movie
 {
-    
+private int id;
+private String title;
+private double IMDBrating;
+private double personalRating;
+private String filelink;
+private Date date;
+
+public Movie(int id, String title, String filelink)
+{
+    this.id=id;
+    this.title=title;
+    this.filelink=filelink;
+}
+
+public Movie(int id, String title, String filelink, double IMDBrating)
+{
+    this.id=id;
+    this.title=title;
+    this.filelink=filelink;
+    this.IMDBrating=IMDBrating;
+}
+
+public String getTitle()
+{
+    return title;
+}
+
+public void setDate(Date date)
+{
+    this.date=date;
+}
+public String getFileLink()
+{
+    return filelink;
+}
+
+public void setPersonalRating(double rating)
+{
+    this.personalRating=rating;
+}
 }
