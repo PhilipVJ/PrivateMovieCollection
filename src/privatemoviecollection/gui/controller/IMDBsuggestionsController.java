7aa6a26f0c57b528c93be05e7a425e6352cdf467
PhilipVJ <44639834+PhilipVJ@users.noreply.github.com>
2019-01-11 09:44:20 +0100

    test

--- src/privatemoviecollection/gui/controller/IMDBsuggestionsController.java
+++ src/privatemoviecollection/gui/controller/IMDBsuggestionsController.java
@@ -12,7 +12,6 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
